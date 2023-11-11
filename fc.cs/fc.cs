3acfc11326fe8ea655ec51c60291ba6a3331cfd4
ADM-olivier <146084234+ADM-olivier@users.noreply.github.com>
2023-11-11 15:16:26 +0000

    this is first c# code in vs studio. i write console.writeline "hello world"

---
+++ fc.cs/fc.cs
@@ -0,0 +1,12 @@
+using systems
+
+namespace first_c#project
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            System.Console.WriteLine("hello world");
+        }
+    }
+}
